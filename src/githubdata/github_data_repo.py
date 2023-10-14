00189a1ed4c6b305b5aaf6c40e2d58d0d9ee4c13
imahdimir <imahdimir@gmail.com>
2023-10-14 21:07:31 +0330

    v15.0.0

--- src/githubdata/github_data_repo.py
+++ src/githubdata/github_data_repo.py
@@ -6,7 +6,6 @@ from pathlib import Path
 
 from giteasy import GitHubRepo
 from mirutil.df import read_data_according_to_type as rdatt
-from mirutil.files import read_json_file as rjf
 
 data_file_suffixes = {
         '.xlsx' : None ,
@@ -31,10 +30,8 @@ class GitHubDataRepo(GitHubRepo) :
                          committing_usr = committing_usr ,
                          token = token)
 
-        self.data_suf = None
-        self.data_fp: Path | list
-        self.meta_fp: Path
-        self.meta: dict
+        self.data_suf: str | None = None
+        self.data_fp: Path | None = None
 
         self.set_data_fps()
 
@@ -42,41 +39,25 @@ class GitHubDataRepo(GitHubRepo) :
         super().clone_overwrite(depth = depth)
         self.set_data_fps()
 
+    def ret_sorted_fpns_by_suf(self , suffix) :
+        ls = list(self.local_path.glob(f'*{suffix}'))
+        return sorted(ls)
+
     def _set_defualt_data_suffix(self) :
         for ky in data_file_suffixes.keys() :
             fps = self.ret_sorted_fpns_by_suf(ky)
-            if len(fps) >= 1 :
+            if len(fps) != 0 :
                 self.data_suf = ky
                 return
 
     def set_data_fps(self) :
         self._set_defualt_data_suffix()
-
         if self.data_suf is None :
             return
-
         fps = self.ret_sorted_fpns_by_suf(self.data_suf)
-
-        if len(fps) == 1 :
-            self.data_fp = fps[0]
-        else :
-            self.data_fp = fps
-
-    def ret_sorted_fpns_by_suf(self , suffix) :
-        ls = list(self.local_path.glob(f'*{suffix}'))
-        return sorted(ls)
-
-    def read_metadata(self) :
-        fps = self.ret_sorted_fpns_by_suf('.json')
-        if len(fps) == 0 :
-            return
-        fp = fps[0]
-        self.meta_fp = fp
-        self.meta = rjf(fp)
-        return self.meta
+        self.data_fp = fps[0]
 
     def read_data(self) :
         if not self.local_path.exists() :
             self.clone_overwrite()
-        if isinstance(self.data_fp , Path) :
-            return rdatt(self.data_fp)
+        return rdatt(self.data_fp)
--- src/githubdata/utils.py
+++ src/githubdata/utils.py
@@ -10,8 +10,8 @@ from persiantools.jdatetime import JalaliDateTime
 
 from .github_data_repo import GitHubDataRepo
 
-def get_data_fr_github_without_double_clone(github_url , remove_cache = False
-                                            ) -> pd.DataFrame :
+def get_data_fr_github_wo_double_clone(github_url , remove_cache = False
+                                       ) -> pd.DataFrame :
     """
     gets data from a GitHub data repo, without cloning it twice.
     if it is already cloned, it will read the data from the local path.
@@ -22,13 +22,11 @@ def get_data_fr_github_without_double_clone(github_url , remove_cache = False
     """
     gd = GitHubDataRepo(github_url)
     df = gd.read_data()
-
     if remove_cache :
         gd.rmdir()
-
     return df
 
-def clone_with_overwrite_a_repo_return_gdr_obj(gd_url) :
+def clone_overwrite_a_repo_return_gdr_obj(gd_url) :
     gdr = GitHubDataRepo(gd_url)
     gdr.clone_overwrite()
     return gdr
